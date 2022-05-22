89bd5cae33ee681c773badd2c2ca6b9b9bec050b
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-05-23 00:51:51 +0800

    Release 0.1.0.1, fix NPE caused by item not initialized

--- src/main/java/chaos/unity/railroad_crossing/core/common/itemgroup/RailroadCrossingItemGroups.java
+++ src/main/java/chaos/unity/railroad_crossing/core/common/itemgroup/RailroadCrossingItemGroups.java
@@ -3,8 +3,9 @@ package chaos.unity.railroad_crossing.core.common.itemgroup;
 import chaos.unity.railroad_crossing.core.common.item.RailroadCrossingItems;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public final class RailroadCrossingItemGroups {
-    public static final ItemGroup RAILROAD_CROSSING_CORE = FabricItemGroupBuilder.build(new Identifier("rc_core", "common"), RailroadCrossingItems.EMITTER_CIRCUIT_ITEM::getDefaultStack);
+    public static final ItemGroup RAILROAD_CROSSING_CORE = FabricItemGroupBuilder.build(new Identifier("rc_core", "common"), () -> new ItemStack(RailroadCrossingItems.EMITTER_CIRCUIT_ITEM));
 }
